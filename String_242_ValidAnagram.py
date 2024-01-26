17523463c89dd9d9632220efbcdc064eb5f1cc39
Akshay Patel <Akshaysukhadiya41@gmail.com>
2024-01-25 22:54:51 -0800

    Update String_242_ValidAnagram.py

--- String_242_ValidAnagram.py
+++ String_242_ValidAnagram.py
@@ -1,4 +1,14 @@
 """LeetCode Problem 242"""
+"""
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+"""
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         s = sorted(s)
